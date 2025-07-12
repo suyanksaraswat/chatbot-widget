7b571c1b283998c62f47b15774d2f3a019120498
Suyank Saraswat <suyank.saraswat@gmail.com>
2025-07-12 23:31:22 +0530

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [{ pattern: /./ }],
   theme: {
     container: {
       center: true,
